bab02136a92b912d2a3dd56030baf4be3aa96cc4
TrungPham <nhokpipuppy@gmail.com>
2019-01-16 09:46:01 +0700

    create file Home.php

---
+++ CodeIgniter/application/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+	class Home extends CI_Controller 
+	{
+		public function __construct()
+		{
+			parent::__construct();
+
+			//load to file HomeModel.php in folder Model
+			$this->load->model('HomeModel');
+		}
+		public function index() {
+			//get data from database and put in var array $data
+			$data['images'] = $this->HomeModel->getImages();
+			// load view and pass data
+			$this->load->view('HomeView',$data);
+		}
+	}
+?>
